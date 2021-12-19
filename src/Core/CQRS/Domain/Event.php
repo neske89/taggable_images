6e9dc95f04c0e0723bd6155a6637668313beb62f
nenad <mail@nenadmilosavljevic.com>
2021-12-20 00:03:58 +0100

    removed event support

--- src/Core/CQRS/Domain/Event.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-namespace App\Core\Messaging\Domain;
-
-interface Event
-{
-}
--- src/Core/CQRS/Domain/EventBus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Core\Messaging\Domain;
-
-interface EventBus
-{
-    public function dispatch(Event $event): void;
-}
